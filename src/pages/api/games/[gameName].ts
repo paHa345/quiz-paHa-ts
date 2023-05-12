d932ad6f1f82109df949babaf088d0fd2951ec5c
paHa345 <pav.345@mail.ru>
2023-05-12 22:34:17 +0700

    v 0.90 fix .env display

--- src/pages/api/games/[gameName].ts
+++ src/pages/api/games/[gameName].ts
@@ -20,7 +20,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   let db;
   try {
     client = await MongoClient.connect(
-      `mongodb://${process.env.mongodb_username}:${process.env.mongodb_password}@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/${process.env.mongodb_database}?replicaSet=rs0`
+      `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/${process.env.MONGODB_DATABASE}?replicaSet=rs0`
     );
     db = client.db();
   } catch (error) {
